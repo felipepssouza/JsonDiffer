3d5f3bb66f13ca606c917bb9205a9abc318536ad
Felipe Souza <felipe@guiabolso.com.br>
2016-12-11 14:10:49 -0200

    Missing method
    Missing method

--- usecase/src/main/java/br/com/felipe/br/com/felipe/map/MapComparator.java
+++ usecase/src/main/java/br/com/felipe/br/com/felipe/map/MapComparator.java
@@ -7,8 +7,11 @@ import java.util.Map;
  */
 public class MapComparator {
 
-
     public boolean isTotallyEqual(Map<String, String> right, Map<String, String> left) {
         return right.equals(left);
+    }
+
+    public boolean isSameSize(Map<String, String> right, Map<String, String> left) {
+        return right.size() == left.size();
     }
 }
